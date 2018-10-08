d7161f5416ae3bfc1dfaf14ad19f3e88a686087d
Kat Perreira <kat.perreira@gmail.com>
2018-10-08 16:04:18 -0700

    generated migrations for vote, work, user

---
+++ db/migrate/20181008224434_create_works.rb
@@ -0,0 +1,12 @@
+class CreateWorks < ActiveRecord::Migration[5.2]
+  def change
+    create_table :works do |t|
+      t.string :category
+      t.string :title
+      t.integer :publication_year
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
