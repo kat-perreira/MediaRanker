27755a9362876b1e9abc23ab826145a823260d35
Kat Perreira <kat.perreira@gmail.com>
2018-10-09 16:35:58 -0700

    modified views, modified root path, sectioned off works into classes

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,62 +1,56 @@
 class WorksController < ApplicationController
 
   def index
-    if params[:work_id]
-      work = Work.find_by(id: params[:work_id])
-      @trips = work.trips
-    else
-      @works = Work.all
-    end
+    @works = Work.all
   end
+end
 
-  def edit
-    @work = Work.find_by(id: params[:id])
-  end
+def edit
+  @work = Work.find_by(id: params[:id])
+end
 
-  def new
-    @work = Work.new
-  end
+def new
+  @work = Work.new
+end
 
-  def destroy
-    work = Work.find_by(id: params[:id])
-    work.destroy
-    redirect_to root_path
-  end
+def destroy
+  work = Work.find_by(id: params[:id])
+  work.destroy
+  redirect_to root_path
+end
 
-  def create
-    filtered_params = work_params()
-    @work = Work.new(filtered_params)
-    save_success = @work.save
-    if save_success
-      redirect_to works_path
-    else
-      render :new
-    end
-  end
-
-  def show
-    work_id = params[:id]
-    @work= Work.find_by(id: work_id)
-    if @work.nil?
-      head :not_found
-    end
-  end
-
-  def update
-    @work = Work.find(params[:id])
-    @work.update(work_params)
+def create
+  filtered_params = work_params()
+  @work = Work.new(filtered_params)
+  save_success = @work.save
+  if save_success
     redirect_to works_path
+  else
+    render :new
   end
+end
 
-  private
-
-  def work_params
-    return params.require(:work).permit(
-      :title,
-      :catagory,
-      :publication_year,
-      :description
-    )
+def show
+  work_id = params[:id]
+  @work= Work.find_by(id: work_id)
+  if @work.nil?
+    head :not_found
   end
+end
 
+def update
+  @work = Work.find(params[:id])
+  @work.update(work_params)
+  redirect_to works_path
+end
+
+private
+
+def work_params
+  return params.require(:work).permit(
+    :title,
+    :catagory,
+    :publication_year,
+    :description
+  )
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'work#index'
+  root 'works#index'
 
-  resources :votes
   resources :works
-  resources :users
+
 
 end
