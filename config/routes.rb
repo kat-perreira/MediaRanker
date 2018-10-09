dd29c205e8c000c3c7a8656c3cc695f3743bf558
Kat Perreira <kat.perreira@gmail.com>
2018-10-08 19:17:21 -0700

    set up temp root_path to works#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources: :works
+  root 'work#index'
 
-  resources: :users
+  resources :votes
+  resources :works
+  resources :users
 
-  resources: :votes
 end
