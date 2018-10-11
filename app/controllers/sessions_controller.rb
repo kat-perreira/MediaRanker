b58d222be8e5c521e1746d19d51d00c1e6acb723
Kat Perreira <kat.perreira@gmail.com>
2018-10-11 14:15:34 -0700

    fixed error with sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,27 +1,2 @@
 class SessionsController < ApplicationController
-
-  def new
-  end
-
-  def create
-    name = params[:username]
-    user = User.new(username: name)
-    user.save
-
-    if user
-      flash[:success] = "Successfully logged in"
-      session[:user_id] = user.id
-      redirect_to root_path
-
-    else
-      render :new
-
-    end
-  end
-
-  def logout
-    session[:user_id] = nil
-    flash[:success] = "You are logged out"
-    redirect_to root_path
-  end
 end
