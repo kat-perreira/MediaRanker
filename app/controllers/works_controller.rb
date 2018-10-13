5df519cc92ac5ce0133167b34d0a543c9183d299
Kat Perreira <kat.perreira@gmail.com>
2018-10-12 20:24:44 -0700

    fixed some bugs within the controllers, put the css back into the scss file

--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -19,38 +19,67 @@ class WorksController < ApplicationController
   end
 
   def create
-    filtered_params = work_params()
-    @work = Work.new(filtered_params)
-    save_success = @work.save
-    if save_success
-      redirect_to works_path
+    title_already_taken = Work.find_by(title: params[:title])
+    if title_already_taken
+      flash[:error]= "A Problem Occured: work with this title already exists"
     else
-      render :new
+      filtered_params = work_params()
+      @work = Work.new(filtered_params)
+      save_success = @work.save
+      if save_success
+        redirect_to works_path
+      else
+        flash.now[:error] =  "Invalid work entry"
+        render :new, status: :bad_request
+      end
     end
   end
 
-  def show
-    work_id = params[:id]
-    @work = Work.find_by(id: work_id)
-    if @work.nil?
-      head :not_found
+    def show
+      work_id = params[:id]
+      @work = Work.find_by(id: work_id)
+      if @work.nil?
+        head :not_found
+      end
+    end
+
+    def update
+      @work = Work.find(params[:id])
+      @work.update(work_params)
+      redirect_to works_path
+    end
+
+    # def upvote
+    #   @user = User.find_by(id: session[:user_id])
+    #
+    #   if @user
+    #     @user.votes.each do |vote|
+    #       if vote.work_id == @work.id
+    #         flash[:error]="This user already voted on this work"
+    #       else
+    #         new_vote = Vote.new
+    #         new_vote.work_id = @work.id
+    #         new_vote.user_id = @user.id
+    #         new_vote.save
+    #         @user.votes << new_vote
+    #       end
+    #     end
+    #   else
+    #     flash[:error] = "You are not logged in"
+    #     redirect_to sessions_path
+    #
+    #   end
+    # end
+
+
+    private
+
+    def work_params
+      return params.require(:work).permit(
+        :title,
+        :catagory,
+        :publication_year,
+        :description
+      )
     end
   end
-
-  def update
-    @work = Work.find(params[:id])
-    @work.update(work_params)
-    redirect_to works_path
-  end
-
-  private
-
-  def work_params
-    return params.require(:work).permit(
-      :title,
-      :catagory,
-      :publication_year,
-      :description
-    )
-  end
-end
