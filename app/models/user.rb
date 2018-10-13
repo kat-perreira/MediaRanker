b4c1487833613133ecf0474cf488c139a182ea78
Kat Perreira <kat.perreira@gmail.com>
2018-10-13 09:09:43 -0700

    set up users show table, put some logic from view into the model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,18 +3,19 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
 
-  # I think business logic for voting goes here, connected with the user
+  # create a vote object with current user's id and the work id passed in
   def vote(work_id)
-    vote =Vote.where(user_id: self.id, work_id: work_id)
+    vote = Vote.where(user_id: self.id, work_id: work_id)
     return vote
   end
 
-  # broke this apart
+  # Assign the list of votes to the variable votes_array...helper method
   def users_votes
     votes_array = Vote.where(user_id: self.id)
     return votes_array
   end
 
+  # collect the work ids for all the works that the user upvoted
   def upvoted
     votes = users_votes
     upvoted_workids = []
@@ -22,6 +23,18 @@ class User < ApplicationRecord
       upvoted_workids << vote
     end
     return upvoted_workids
+  end
+
+  # return the works users voted on here, not in the view.
+
+  def user_voted_works
+    work_ids = upvoted
+    works = []
+    work_ids.each do |id|
+      work = Work.find_by(id: id)
+      works << work
+    end
+    return works
   end
 
 
