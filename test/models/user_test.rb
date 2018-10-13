3aab6e4f8a193b1505021151d4ced4a17e1a1f32
Kat Perreira <kat.perreira@gmail.com>
2018-10-13 07:30:05 -0700

    passed relations test for users

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,9 +1,20 @@
 require "test_helper"
 
 describe User do
-  let(:user) { User.new }
 
-  it "must be valid" do
-    value(user).must_be :valid?
+  describe 'has relations' do
+    it 'a user can have votes' do
+      user = users.first
+      work_first = works.first
+      work_second = works.second
+
+      vote = Vote.create!(user_id: user.id, work_id: work_first.id)
+      vote = Vote.create!(user_id: user.id, work_id: work_second.id)
+      two_votes = Vote.where(user_id: user.id)
+
+      expect(two_votes.count).must_equal 2
+    end
   end
+
+
 end
